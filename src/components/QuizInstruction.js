e297ea1c721854fb78b589ca1fdd05bc31be733c
rksh1995 <rakeshsuman1920@gmail.com>
2023-09-24 11:46:55 +0530

    added style to quizList and quizInstruction

--- src/components/QuizInstruction.js
+++ src/components/QuizInstruction.js
@@ -1,22 +1,55 @@
-
 import { useNavigate } from "react-router-dom";
-const QuizInstruction = ()=>{
 
-    const navigate = useNavigate();
-    return(
-        <div>
-            <h2>Quiz App Instructions</h2>
-            <ol type="1">
-                <li>Each quiz contains 25-40 questions</li>
-                <li>You get 1 point for each correct answer</li>
-                <li>Every Question have four option</li>
-                <li>No negative marking are there</li>
-                <li>Your device camera should always on during quiz</li>
-                <li>Your internet speed must be 20 mbps</li>
-                
-            </ol>
-            <button onClick={ () => navigate("/quizList")}>Star Quiz</button>
-        </div>
-    )
-}
+const QuizInstruction = () => {
+  const navigate = useNavigate();
+
+  const containerStyle = {
+    textAlign: 'center',
+    padding: '20px',
+  };
+
+  const headingStyle = {
+    fontSize: '24px',
+    fontWeight: 'bold',
+    marginBottom: '20px',
+  };
+
+  const listStyle = {
+    textAlign: 'left',
+    paddingLeft: '20px',
+  };
+
+  const listItemStyle = {
+    fontSize: '18px',
+    marginBottom: '10px',
+  };
+
+  const buttonStyle = {
+    backgroundColor: '#007bff',
+    color: '#fff',
+    padding: '10px 20px',
+    border: 'none',
+    borderRadius: '5px',
+    margin: '10px',
+    cursor: 'pointer',
+  };
+
+  return (
+    <div style={containerStyle}>
+      <h2 style={headingStyle}>Quiz App Instructions</h2>
+      <ol type="1" style={listStyle}>
+        <li style={listItemStyle}>Each quiz contains 25-40 questions</li>
+        <li style={listItemStyle}>You get 1 point for each correct answer</li>
+        <li style={listItemStyle}>Every Question has four options</li>
+        <li style={listItemStyle}>No negative marking is there</li>
+        <li style={listItemStyle}>Your device camera should always be on during the quiz</li>
+        <li style={listItemStyle}>Your internet speed must be 20 mbps</li>
+      </ol>
+      <button style={buttonStyle} onClick={() => navigate("/quizList")}>
+        Start Quiz
+      </button>
+    </div>
+  );
+};
+
 export default QuizInstruction;
--- src/components/QuizList.js
+++ src/components/QuizList.js
@@ -1,29 +1,62 @@
 
 // import { useNavigate } from "react-router-dom";
-const QuizList = ()=>{
+
 
     // const navigate = useNavigate();
-    return(
-        <div>
-          
-            <h2>Quiz List</h2>
-            <ul>
-                <li>HTML Quiz</li>
-                <li>CSS Quiz</li>
-                <li>JAVASCRIPT Quiz</li>
-                <li>REACT JS Quiz</li>
-                <li>BOOTSTRAP Quiz</li>
-                <li>JQUERY Quiz</li>
-                <li>NODE JS Quiz</li>
-                <li>SQL Quiz</li>
-                <li>PHP Quiz</li>
-                <li>PYTHO Quiz</li>
-            </ul>
-            
-            <button>Prev</button>
-        <button>Next</button>
-        </div>
+
+    const QuizList = () => {
+        const containerStyle = {
+          textAlign: 'center',
+          padding: '20px',
+        };
       
-    )
-}
-export default QuizList;
+        const headingStyle = {
+          fontSize: '24px',
+          fontWeight: 'bold',
+          marginBottom: '20px',
+        };
+      
+        const listStyle = {
+          listStyleType: 'none',
+          padding: '0',
+        };
+      
+        const listItemStyle = {
+          fontSize: '18px',
+          marginBottom: '10px',
+        };
+      
+        const buttonStyle = {
+          backgroundColor: '#007bff',
+          color: '#fff',
+          padding: '10px 20px',
+          border: 'none',
+          borderRadius: '5px',
+          margin: '10px',
+          cursor: 'pointer',
+        };
+      
+        return (
+          <div style={containerStyle}>
+            <h2 style={headingStyle}>Quiz List</h2>
+            <ul style={listStyle}>
+              <li style={listItemStyle}>HTML Quiz</li>
+              <li style={listItemStyle}>CSS Quiz</li>
+              <li style={listItemStyle}>JAVASCRIPT Quiz</li>
+              <li style={listItemStyle}>REACT JS Quiz</li>
+              <li style={listItemStyle}>BOOTSTRAP Quiz</li>
+              <li style={listItemStyle}>JQUERY Quiz</li>
+              <li style={listItemStyle}>NODE JS Quiz</li>
+              <li style={listItemStyle}>SQL Quiz</li>
+              <li style={listItemStyle}>PHP Quiz</li>
+              <li style={listItemStyle}>PYTHON Quiz</li>
+            </ul>
+            <button style={buttonStyle}>Prev</button>
+            <button style={buttonStyle}>Next</button>
+          </div>
+        );
+      };
+      
+      export default QuizList;
+      
+    
